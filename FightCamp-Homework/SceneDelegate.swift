7bdb548bfe7e608a5611cfda75f384ac0f2af43f
Jenny Luu <luujenny26@gmail.com>
2020-11-08 21:08:43 -0800

    Make PackagesViewController the root view controller

--- FightCamp-Homework/SceneDelegate.swift
+++ FightCamp-Homework/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         window = .init(windowScene: windowScene)
-        window?.rootViewController = nil /* instantiate the view controller here */
+        window?.rootViewController = PackagesViewController()
         window?.makeKeyAndVisible()
     }
 
